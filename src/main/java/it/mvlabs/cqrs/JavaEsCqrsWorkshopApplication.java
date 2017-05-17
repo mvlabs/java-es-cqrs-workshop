c295a8d6f67f684d7bd2232fea4566f03628380f
upescatore <ubaldopescatore@gmail.com>
2017-05-17 20:13:41 +0200

    Clean setup

---
+++ src/main/java/it/mvlabs/cqrs/JavaEsCqrsWorkshopApplication.java
@@ -0,0 +1,12 @@
+package it.mvlabs.cqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEsCqrsWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEsCqrsWorkshopApplication.class, args);
+	}
+}

a3c9f146018e207513854acbbc7aaa0107cd8854
upescatore <ubaldopescatore@gmail.com>
2017-05-25 23:17:55 +0200

    code cleaned for workshop step 4

--- src/main/java/it/mvlabs/cqrs/query/PizzeriaEventListener.java
+++ src/main/java/it/mvlabs/cqrs/query/PizzeriaEventListener.java
@@ -1,15 +1,10 @@
 package it.mvlabs.cqrs.query;
 
-import java.util.List;
-
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import it.mvlabs.cqrs.events.OrderAddedEvent;
 import it.mvlabs.cqrs.events.PizzeriaCreatedEvent;
-import it.mvlabs.hibernate.JSONBOrder;
-import it.mvlabs.hibernate.JSONUtility;
 
 @Component
 public class PizzeriaEventListener {
@@ -29,18 +24,7 @@ public class PizzeriaEventListener {
                 "[]"));
     }
 
-    @EventHandler
-    public void on(OrderAddedEvent event) {
-        PizzeriaEntry pizzeria = repository.findOne(event.getPizzeriaId());
-        
-        JSONBOrder order = new JSONBOrder(
-                event.getAt(), 
-                event.getCustomerName(), 
-                event.getPizzaTaste());  
-        List<JSONBOrder> orders = JSONUtility.ordersFromJsonArray(pizzeria.getPizzas());
-        orders.add(order);
-        
-        pizzeria.setPizzas(JSONUtility.serializeOrders(orders));
-        repository.save(pizzeria);
-    }
+  //TODO add reading projection here for order added event
+
+   
 }

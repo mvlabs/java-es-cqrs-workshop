ea8bd6899f55b4f3863826a38d15e60495c88cb7
upescatore <ubaldopescatore@gmail.com>
2017-05-26 00:03:26 +0200

    cleaned up code for step 0, workshop setup ok

--- src/main/java/it/mvlabs/cqrs/JavaEsCqrsWorkshopApplication.java
+++ src/main/java/it/mvlabs/cqrs/JavaEsCqrsWorkshopApplication.java
@@ -2,9 +2,21 @@ package it.mvlabs.cqrs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class JavaEsCqrsWorkshopApplication {
+    
+    @RequestMapping("/")
+    @ResponseBody
+    public ResponseEntity<String> loadAllPizzerias() {
+        return new ResponseEntity<>("Please checkout 1-create-pizzeria branch!", HttpStatus.ACCEPTED);
+    } 
     
 	public static void main(String[] args) {
 		SpringApplication.run(JavaEsCqrsWorkshopApplication.class, args);
--- src/main/java/it/mvlabs/cqrs/query/PizzeriaRepository.java
+++
@@ -1,8 +0,0 @@
-package it.mvlabs.cqrs.query;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PizzeriaRepository extends JpaRepository<PizzeriaEntry, String> {
-}
